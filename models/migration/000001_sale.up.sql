8c5e0f529a00702ba66a73190ca4a588224128ae
nylespham <nylespham@gmail.com>
2026-04-15 22:35:21 +0700

    IM-22 Update sqlc model

--- models/migration/000001_sale.up.sql
+++ models/migration/000001_sale.up.sql
@@ -1,15 +1,8 @@
-CREATE TABLE "sale_unit" (
+CREATE TABLE "sale" (
   "id" bigserial PRIMARY KEY,
-  "name" varchar NOT NULL,
   "pos_id" int NOT NULL,
   "price" int NOT NULL,
-  "sale_recipe_id" int NOT NULL,
+  "recipe_id" int NOT NULL,
+  "order_id" int NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
-
-CREATE TABLE "sale_recipe" (
-  "id" bigserial PRIMARY KEY,
-  "ingredients" int[] NOT NULL
-);
-
-ALTER TABLE "sale_unit" ADD FOREIGN KEY ("sale_recipe_id") REFERENCES "sale_recipe" ("id");
--- models/query/sale.unit.sql
+++ models/query/sale.unit.sql
@@ -1,28 +1,29 @@
 -- name: CreateSaleUnit :one
-INSERT INTO sale_unit (
-    name, pos_id, price, sale_recipe_id
+INSERT INTO sale (
+    pos_id, price, recipe_id, order_id
 ) VALUES (
     $1, $2, $3, $4
 ) RETURNING *;
 
 -- name: UpdateSaleUnit :one
-UPDATE sale_unit
-SET name = $2,
-    pos_id = $3,
-    price = $4,
-    sale_recipe_id = $5
+UPDATE sale
+SET 
+    pos_id = $2,
+    price = $3,
+    recipe_id = $4,
+    order_id = $5
 WHERE id = $1
 RETURNING *;
 
 -- name: GetSaleUnit :one
-SELECT * FROM sale_unit
+SELECT * FROM sale
 WHERE id = $1;
 
 -- name: ListSaleUnit :many
-SELECT id, name, pos_id, price, sale_recipe_id
-FROM sale_unit
+SELECT id, pos_id, order_id, price, recipe_id
+FROM sale
 LIMIT $1 OFFSET $2;
 
 -- name: DeleteSaleUnit :exec
-DELETE FROM sale_unit
+DELETE FROM sale
 WHERE id = $1;
